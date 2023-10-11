1ab93396399cd88467c2404d6fa31bb296a7958a
Netz9 <nilmar2024@gmail.com>
2023-10-11 13:00:54 -0600

    Primer Commit

---
+++ src/router/index.js
@@ -0,0 +1,535 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+/* Layouts */
+import Layout1 from '../layouts/Layout1'
+import AuthLayout1 from '../layouts/AuthLayouts/AuthLayout1'
+/* Dashboards View */
+import Dashboard2 from '../views/Dashboards/Dashboard2.vue'
+import Dashboard1 from '../views/Dashboards/Dashboard1.vue'
+import Dashboard3 from '../views/Dashboards/Dashboard3'
+import Dashboard4 from '../views/Dashboards/Dashboard4'
+import Dashboard5 from '../views/Dashboards/Dashboard5'
+/* UIElements View */
+import UiAlerts from '../views/core/UiAlerts.vue'
+import UiButtons from '../views/core/UiButtons.vue'
+import UiBadges from '../views/core/UiBadges.vue'
+import UiBreadcrumb from '../views/core/UiBreadcrumb.vue'
+import UiColors from '../views/core/UiColors.vue'
+import UiTypography from '../views/core/UiTypography.vue'
+import UiCards from '../views/core/UiCards.vue'
+import UiCarousel from '../views/core/UiCarousel.vue'
+import UiEmbedVideo from '../views/core/UiEmbedVideo.vue'
+import UiGrid from '../views/core/UiGrid.vue'
+import UiModal from '../views/core/UiModal.vue'
+import UiListGroup from '../views/core/UiListGroup.vue'
+import UiImages from '../views/core/UiImages.vue'
+import UiMediaObject from '../views/core/UiMediaObject.vue'
+import UiTooltips from '../views/core/UiTooltips.vue'
+import UiPopovers from '../views/core/UiPopovers.vue'
+import UiNotifications from '../views/core/UiNotifications.vue'
+import UiTabs from '../views/core/UiTabs.vue'
+import UiPagination from '../views/core/UiPagination.vue'
+import UiProgressBars from '../views/core/UiProgressBars.vue'
+/* Authentic View */
+import SignIn1 from '../views/AuthPages/Default/SignIn1'
+import SignUp1 from '../views/AuthPages/Default/SignUp1'
+import RecoverPassword1 from '../views/AuthPages/Default/RecoverPassword1'
+import LockScreen1 from '../views/AuthPages/Default/LockScreen1'
+import ConfirmMail1 from '../views/AuthPages/Default/ConfirmMail1'
+/* Icon Views */
+import IconDripicons from '../views/Icons/IconDripicons'
+import IconFontawesome5 from '../views/Icons/IconFontawesome5'
+import IconLineAwesome from '../views/Icons/IconLineAwesome'
+import IconRemixicon from '../views/Icons/IconRemixicon'
+import IconUnicons from '../views/Icons/IconUnicons'
+
+import ApexCharts from '../views/Charts/ApexCharts'
+import AmCharts from '../views/Charts/AmCharts'
+import MorrisCharts from '../views/Charts/MorrisCharts'
+import HighCharts from '../views/Charts/HighCharts'
+
+/* Form View */
+import FormLayout from '../views/Forms/FormLayout'
+import FormValidates from '../views/Forms/FormValidates'
+import FormSwitches from '../views/Forms/FormSwitches'
+import FormRadios from '../views/Forms/FormRadios'
+import FormCheckboxes from '../views/Forms/FormCheckboxes'
+/* User View */
+import Profile from '../views/User/Profile'
+import ProfileEdit from '../views/User/ProfileEdit'
+import AddUser from '../views/User/AddUser'
+import UserList from '../views/User/UserList'
+/* Todo */
+import Callback from '../views/AuthPages/Default/Callback'
+
+// App views
+// gerencia
+import AdminParent from '../views/App/Admin/AdminParent'
+import Asuetos from '../views/App/Admin/Asuetos.vue'
+import Cuartos from '../views/App/Admin/Cuartos.vue'
+import Users from '../views/App/Admin/Users'
+
+// caja
+import CortesParent from '../views/App/Caja/Cortes/CortesParent'
+import CuentasParent from '../views/App/Caja/Cuentas/CuentasParent'
+import EgresosParent from '../views/App/Caja/Egresos/EgresosParent'
+
+import AlumnosList from '../views/App/Alumnos/alumnoList'
+
+Vue.use(VueRouter)
+
+const AdminRoutes = (prop, mode) => [
+  {
+    path: 'users',
+    name: prop + '.users',
+    meta: { dark: mode, auth: true, name: 'Usuarios' },
+    component: Users
+  },
+  {
+    path: 'cuartos',
+    name: prop + '.cuartos',
+    meta: { dark: mode, auth: true, name: 'Cuartos' },
+    component: Cuartos
+  },
+  {
+    path: 'asuetos',
+    name: prop + '.asuetos',
+    meta: { dark: mode, auth: true, name: 'Asuetos' },
+    component: Asuetos
+  },
+  {
+    path: 'adminparent',
+    name: prop + '.adminparent',
+    meta: { dark: mode, auth: true, name: 'adminparent' },
+    component: AdminParent
+  }
+]
+
+const AlumnoRoutes = (prop, mode) => [
+  {
+    path: 'alumno',
+    name: prop + '.alumnos',
+    meta: { dark: mode, auth: true, name: 'alumnos' },
+    component: AlumnosList
+  },
+  {
+    path: 'cortes',
+    name: prop + '.cortes',
+    meta: { dark: mode, auth: true, name: 'cortes' },
+    component: CortesParent
+  },
+  {
+    path: 'cuentas',
+    name: prop + '.cuentas',
+    meta: { dark: mode, auth: true, name: 'cuentas' },
+    component: CuentasParent
+  },
+  {
+    path: 'egresos',
+    name: prop + '.egresos',
+    meta: { dark: mode, auth: true, name: 'egresos' },
+    component: EgresosParent
+  }
+]
+
+const childRoutes = (prop, mode) => [
+  {
+    path: 'home-1',
+    name: prop + '.home-1',
+    meta: { dark: mode, auth: true, name: 'Home 1' },
+    component: Dashboard1
+  },
+  {
+    path: '`home-2`',
+    name: prop + '.home-2',
+    meta: { dark: mode, auth: true, name: 'Home 2' },
+    component: Dashboard2
+  },
+  {
+    path: 'home-3',
+    name: prop + '.home-3',
+    meta: { dark: mode, auth: true, name: 'Home 3' },
+    component: Dashboard3
+  },
+  {
+    path: 'home-5',
+    name: prop + '.home-5',
+    meta: { dark: mode, auth: true, name: 'Home 5' },
+    component: Dashboard5
+  },
+  {
+    path: '/home-4',
+    name: prop + '.home-4',
+    meta: { dark: mode, auth: true, name: 'Home 4' },
+    component: Dashboard4
+  }
+]
+
+const coreChildRoute = (prop, mode) => [
+  {
+    path: 'ui-colors',
+    name: prop + '.color',
+    meta: { dark: mode, auth: true, name: 'Colors' },
+    component: UiColors
+  },
+  {
+    path: 'ui-typography',
+    name: prop + '.typography',
+    meta: { dark: mode, auth: true, name: 'Typography' },
+    component: UiTypography
+  },
+  {
+    path: 'ui-alerts',
+    name: prop + '.alert',
+    meta: { dark: mode, auth: true, name: 'Alert' },
+    component: UiAlerts
+  },
+  {
+    path: 'ui-buttons',
+    name: prop + '.button',
+    meta: { dark: mode, auth: true, name: 'Button' },
+    component: UiButtons
+  },
+  {
+    path: 'ui-breadcrumb',
+    name: prop + '.breadcrumb',
+    meta: { dark: mode, auth: true, name: 'Breadcrumb' },
+    component: UiBreadcrumb
+  },
+  {
+    path: 'ui-badges',
+    name: prop + '.badges',
+    meta: { dark: mode, auth: true, name: 'Badges' },
+    component: UiBadges
+  },
+  {
+    path: 'ui-cards',
+    name: prop + '.cards',
+    meta: { dark: mode, auth: true, name: 'Card' },
+    component: UiCards
+  },
+  {
+    path: 'ui-carousel',
+    name: prop + '.carousel',
+    meta: { dark: mode, auth: true, name: 'Carousel' },
+    component: UiCarousel
+  },
+  {
+    path: 'ui-grid',
+    name: prop + '.grid',
+    meta: { dark: mode, auth: true, name: 'Grid' },
+    component: UiGrid
+  },
+  {
+    path: 'ui-embed-video',
+    name: prop + '.embed-video',
+    meta: { dark: mode, auth: true, name: 'Embed Video' },
+    component: UiEmbedVideo
+  },
+  {
+    path: 'ui-modal',
+    name: prop + '.modal',
+    meta: { dark: mode, auth: true, name: 'Model' },
+    component: UiModal
+  },
+  {
+    path: 'ui-listgroup',
+    name: prop + '.listgroup',
+    meta: { dark: mode, auth: true, name: 'List Group' },
+    component: UiListGroup
+  },
+  {
+    path: 'ui-images',
+    name: prop + '.images',
+    meta: { dark: mode, auth: true, name: 'Image' },
+    component: UiImages
+  },
+  {
+    path: 'ui-media-object',
+    name: prop + '.media-object',
+    meta: { dark: mode, auth: true, name: 'Media Object' },
+    component: UiMediaObject
+  },
+  {
+    path: 'ui-pagination',
+    name: prop + '.pagination',
+    meta: { dark: mode, auth: true, name: 'Paginations' },
+    component: UiPagination
+  },
+  {
+    path: 'ui-progressbars',
+    name: prop + '.progressbars',
+    meta: { dark: mode, auth: true, name: 'Progressbar' },
+    component: UiProgressBars
+  },
+  {
+    path: 'ui-tooltips',
+    name: prop + '.tooltips',
+    meta: { dark: mode, auth: true, name: 'Tooltip' },
+    component: UiTooltips
+  },
+  {
+    path: 'ui-popovers',
+    name: prop + '.popovers',
+    meta: { dark: mode, auth: true, name: 'Popover' },
+    component: UiPopovers
+  },
+  {
+    path: 'ui-notifications',
+    name: prop + '.notifications',
+    meta: { dark: mode, auth: true, name: 'Notification' },
+    component: UiNotifications
+  },
+  {
+    path: 'ui-tabs',
+    name: prop + '.tabs',
+    meta: { dark: mode, auth: true, name: 'Tabs' },
+    component: UiTabs
+  }
+]
+const formChildRoute = (prop, mode = false) => [
+  {
+    path: 'form-layout',
+    name: prop + '.layout',
+    meta: { dark: mode, auth: true, name: 'Layout' },
+    component: FormLayout
+  },
+  {
+    path: 'form-validate',
+    name: prop + '.validate',
+    meta: { dark: mode, auth: true, name: 'Validate' },
+    component: FormValidates
+  },
+  {
+    path: 'form-switches',
+    name: prop + '.switch',
+    meta: { dark: mode, auth: true, name: 'Switch' },
+    component: FormSwitches
+  },
+  {
+    path: 'form-radios',
+    name: prop + '.radio',
+    meta: { dark: mode, auth: true, name: 'Radio' },
+    component: FormRadios
+  },
+  {
+    path: 'form-checkboxes',
+    name: prop + '.checkbox',
+    meta: { dark: mode, auth: true, name: 'Checkbox' },
+    component: FormCheckboxes
+  }
+]
+
+const iconChildRoute = (prop, mode = false) => [
+  {
+    path: 'dripicons',
+    name: prop + '.dripicons',
+    meta: { dark: mode, auth: true, name: 'Dripicons' },
+    component: IconDripicons
+  },
+  {
+    path: 'fontawesome-5',
+    name: prop + '.fontawesome-5',
+    meta: { dark: mode, auth: true, name: 'Font Awsome' },
+    component: IconFontawesome5
+  },
+  {
+    path: 'lineawesome',
+    name: prop + '.lineawesome',
+    meta: { dark: mode, auth: true, name: 'Line Awsome' },
+    component: IconLineAwesome
+  },
+  {
+    path: 'remixicon',
+    name: prop + '.remixicon',
+    meta: { dark: mode, auth: true, name: 'Remixicon' },
+    component: IconRemixicon
+  },
+  {
+    path: 'unicons',
+    name: prop + '.unicons',
+    meta: { dark: mode, auth: true, name: 'Unicon' },
+    component: IconUnicons
+  }
+]
+
+const chartChildRoutes = (prop, mode = false) => [
+  {
+    path: 'apex-charts',
+    name: prop + '.apex',
+    meta: { dark: mode, auth: true, name: 'Apex Chat' },
+    component: ApexCharts
+  },
+  {
+    path: 'am-charts',
+    name: prop + '.am',
+    meta: { dark: mode, auth: true, name: 'Am Chart' },
+    component: AmCharts
+  },
+  {
+    path: 'high-charts',
+    name: prop + '.high',
+    meta: { dark: mode, auth: true, name: 'High Chart' },
+    component: HighCharts
+  },
+  {
+    path: 'morris-charts',
+    name: prop + '.morris',
+    meta: { dark: mode, auth: true, name: 'Mirris Chart' },
+    component: MorrisCharts
+  }
+]
+
+const authChildRoutes = (prop, mode = false) => [
+  {
+    path: 'sign-in1',
+    name: prop + '.sign-in1',
+    meta: { dark: mode, auth: true },
+    component: SignIn1
+  },
+  {
+    path: 'sign-up1',
+    name: prop + '.sign-up1',
+    meta: { dark: mode, auth: true },
+    component: SignUp1
+  },
+  {
+    path: 'password-reset1',
+    name: prop + '.password-reset1',
+    meta: { dark: mode, auth: true },
+    component: RecoverPassword1
+  },
+  {
+    path: 'lock-screen1',
+    name: prop + '.lock-screen1',
+    meta: { dark: mode, auth: true },
+    component: LockScreen1
+  },
+  {
+    path: 'confirm-mail1',
+    name: prop + '.confirm-mail1',
+    meta: { dark: mode, auth: true },
+    component: ConfirmMail1
+  }
+]
+const authChildRoutes2 = (prop, mode = false) => [
+  {
+    path: '/',
+    name: prop + '.login',
+    meta: { dark: mode, auth: true },
+    component: SignIn1
+  }
+]
+const userChildRoute = (prop, mode = false) => [
+  {
+    path: 'profile',
+    name: prop + '.profile',
+    meta: { dark: mode, auth: true, name: 'Profile' },
+    component: Profile
+  },
+  {
+    path: 'profile-edit',
+    name: prop + '.edit',
+    meta: { dark: mode, auth: true, name: 'Edit Profile' },
+    component: ProfileEdit
+  },
+  {
+    path: 'add-user',
+    name: prop + '.add',
+    meta: { dark: mode, auth: true, name: 'Add Profile' },
+    component: AddUser
+  },
+  {
+    path: 'user-list',
+    name: prop + '.list',
+    meta: { dark: mode, auth: true, name: 'User List' },
+    component: UserList
+  }
+]
+
+const routes = [
+  {
+    path: '/gerencia',
+    name: 'gerencia',
+    component: Layout1,
+    meta: { auth: true },
+    children: AdminRoutes('gerencia')
+  },
+  {
+    path: '/alumno',
+    name: 'alumno',
+    component: Layout1,
+    meta: { auth: true },
+    children: AlumnoRoutes('alumno')
+  },
+  {
+    path: '/',
+    name: 'login',
+    component: AuthLayout1,
+    meta: { auth: true },
+    children: authChildRoutes2('login')
+  },
+  {
+    path: '/',
+    name: 'dashboard',
+    component: Layout1,
+    meta: { auth: true },
+    children: childRoutes('dashboard')
+  },
+  {
+    path: '/core',
+    name: 'core',
+    component: Layout1,
+    meta: { auth: true },
+    children: coreChildRoute('core')
+  },
+  {
+    path: '/chart',
+    name: 'chart',
+    component: Layout1,
+    meta: { auth: true },
+    children: chartChildRoutes('chart')
+  },
+  {
+    path: '/icon',
+    name: 'icon',
+    component: Layout1,
+    meta: { auth: true },
+    children: iconChildRoute('icon')
+  },
+  {
+    path: '/auth',
+    name: 'auth1',
+    component: AuthLayout1,
+    meta: { auth: true },
+    children: authChildRoutes('auth1')
+  },
+  {
+    path: '/doctor',
+    name: 'doctor',
+    component: Layout1,
+    meta: { auth: true },
+    children: userChildRoute('doctor')
+  },
+  {
+    path: '/form',
+    name: 'form',
+    component: Layout1,
+    meta: { auth: true },
+    children: formChildRoute('form')
+  },
+  {
+    path: '/callback',
+    name: 'callback',
+    meta: { auth: false },
+    component: Callback
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.VUE_APP_BASE_URL,
+  routes
+})
+
+export default router
